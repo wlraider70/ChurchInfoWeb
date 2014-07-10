752ddadaaaae58e87bd3fa5315c829e212b06030
Luke Monahan <wlradier70@hotmail.com>
2014-07-09 23:17:02 -0400

    Livesearch folder is used in the jquery search done my Checkin.php.

---
+++ livesearch/index.php
@@ -0,0 +1,22 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta content="text/html; charset=UTF-8" http-equiv="Content-Type"/>
+<title>Live Search Using Jquery,Mysql and PHP</title>
+<link href="css/style.css" type="text/css" rel="stylesheet"/>
+<link href="css/reset.css" type="text/css" rel="stylesheet"/>
+<script type="text/javascript" src="js/jquery-1.3.2.js"></script>
+<script type="text/javascript" src="js/jquery.watermark.js"></script>
+<script type="text/javascript">
+
+
+</html>
+
+
+<form action="search.php" method="post">
+	Enter your zipcode:
+	<input type="text" id="zipsearch" />
+ 
+	<br />
+	<input type="submit" value="Search" />
+</form>
+
---
+++ livesearch/suggest_zip.php
@@ -0,0 +1,24 @@
+<?php
+
+if ( !isset($_REQUEST['term']) )
+    exit;
+
+$dblink = mysql_connect('localhost', 'root', 'clc2net!') or die( mysql_error() );
+mysql_select_db('cla-constituents');
+
+$rs = mysql_query('select per_FirstName, per_LastName, per_ID from person_per where per_FirstName or per_LastName like "'. mysql_real_escape_string($_REQUEST['term']) .'%" order by per_FirstName asc limit 0,10', $dblink);
+
+$data = array();
+if ( $rs && mysql_num_rows($rs) )
+{
+    while( $row = mysql_fetch_array($rs, MYSQL_ASSOC) )
+    {
+        $data[] = array(
+            'label' => $row['per_FirstName'] .', '. $row['per_LastName'] .' '. $row['per_ID'] ,
+            'value' => $row['per_FirstName']
+        );
+    }
+}
+
+echo json_encode($data);
+flush();
